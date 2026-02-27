5d8add19c795f52ead41d3f0f4d77ef6793d04ea
geoffrey <geoffreyembuscado@jit.ph>
2026-02-27 16:02:53 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'restore-smart-ecobin-app',
+  webDir: 'www'
+};
+
+export default config;
